406165970b73fd5776143c6198edc61c7f1ac05d
Faisal Ahammed <50483833+faisalahammed044274@users.noreply.github.com>
2021-07-12 01:45:58 +0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -25,14 +25,14 @@ require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Turn On The Lights
+| Turn On The Lights         faisalahammed044274/laravel56 twitter: setufaisal
 |--------------------------------------------------------------------------
 |
 | We need to illuminate PHP development, so let us turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it
 | will load up this application so that we can run it and send
 | the responses back to the browser and delight our users.
-|
+|       
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
